c120a9b7e4a1d7e77853eee142e017432f785af5
amaur <amaur@Monke-gamer>
2026-05-05 15:22:46 +0200

    test commit et push depuis amaury

--- java_projet/src/java_projet/Test.java
+++ java_projet/src/java_projet/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("aller");
 		System.out.println("alal");
+		// test push depuis amaury
+		System.out.println("test amaur");
 
 	}
 
